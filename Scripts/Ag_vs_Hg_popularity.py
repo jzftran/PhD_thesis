f8484f13f80fec551b9b2f93c0b453d207ec8add
Józef Tran <263396@uwr.edu.pl>
2023-01-25 11:03:22 +0100

    changes in agRad2

--- Scripts/Ag_vs_Hg_popularity.py
+++ Scripts/Ag_vs_Hg_popularity.py
@@ -1,10 +1,11 @@
 #%%
-import urllib
+import urllib.request
 from bs4 import BeautifulSoup
 import matplotlib as plt
 import pandas as pd
 
 def pubs_by_year(link):
+    """Fetches count of publications published in Pubmed in each year based on search query."""
     page = urllib.request.urlopen(link)
     htmlSource = BeautifulSoup(page, "html.parser")
     counts = {"counts":{}}
@@ -19,13 +20,13 @@ def pubs_by_year(link):
 
 
 
-Ag_toxicity_query = "https://pubmed.ncbi.nlm.nih.gov/?term=silver+toxicity"
-Hg_toxicity_query = "https://pubmed.ncbi.nlm.nih.gov/?term=mercury+toxicity"
+Ag_toxicity_query = 'https://pubmed.ncbi.nlm.nih.gov/?term="silver+toxicity"'
+Hg_toxicity_query = 'https://pubmed.ncbi.nlm.nih.gov/?term="mercury+toxicity"'
 ag = pubs_by_year(Ag_toxicity_query)
 hg = pubs_by_year(Hg_toxicity_query)
 #%%
-ag = pd.DataFrame(ag).rename({'counts':'Search query: silver toxicity'},axis=1)
-hg = pd.DataFrame(hg).rename({'counts':'Search query: mercury toxicity'},axis=1)
+ag = pd.DataFrame(ag).rename({'counts':'Search query: "silver toxicity"'},axis=1)
+hg = pd.DataFrame(hg).rename({'counts':'Search query: "mercury toxicity"'},axis=1)
 ag_hg = pd.concat([ag,hg], axis=1)
 ag_hg = ag_hg.iloc[1: , :].fillna(0)
 ag_hg = ag_hg.astype('int')
@@ -34,10 +35,7 @@ ag_hg.sort_index(inplace=True)
 ax = ag_hg.plot(style=('o-'), markersize=4)
 ax.set_xlabel("Year")
 ax.set_ylabel("Number of results")
-ax.axvline(x=77, linestyle='--', color='black')
+ax.axvline(x=64, linestyle='--', color='black')
 # %%
 fig = ax.get_figure()
-fig.savefig('../gfx/ag_hg_count.png',transparent=True, dpi=300)
-
-
-# %%
+fig.savefig('./gfx/ag_hg_count.png',transparent=True, dpi=300)
